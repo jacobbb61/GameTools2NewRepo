7893c045caedac36b19c5a4d8b8cfd41eb0f5a71
jacobbb61 <mcconnelljacob61@gmail.com>
2023-01-13 18:05:02 +0000

    no

--- Big Sword/Assets/scripts/Boss scripts/SewerBossCombat.cs
+++ Big Sword/Assets/scripts/Boss scripts/SewerBossCombat.cs
@@ -39,23 +39,28 @@ public class SewerBossCombat : MonoBehaviour
         HPBar.transform.localScale = new Vector3(0.5f, 0.1f, HP);
 
 
+        // change sword colliders to be bigger
 
         
 
-            if (AtkT > 0f) { AtkT -= Time.deltaTime; } else { navMeshAgent.speed = 2f; navMeshAgent.angularSpeed = 360f; attack3 = false; }
+            if (AtkT > 0f) { AtkT -= Time.deltaTime; } 
+            else 
+            {  attack3 = false; }
+            if (AtkT <= 0f && PhaseChange==false) { navMeshAgent.speed = 2f; navMeshAgent.angularSpeed = 360f; }
+            if (AtkT <= 0f && PhaseChange==true) { navMeshAgent.speed = 4f; navMeshAgent.angularSpeed = 800f; }
             if (AtkT > 1f && AtkT < 1.5f && attack3 == true) { navMeshAgent.angularSpeed = 6000f; }
             if (AtkT > 0.9f && AtkT < 1.2f && attack3 == true) { navMeshAgent.speed = 500f; }
             if (AtkT < 0.9f && attack3 == true) { navMeshAgent.speed = 0f; navMeshAgent.angularSpeed = 0f; }
 
 
-        if ((PhaseChange == false) && (AtkT <= 0f) && (Vector3.Distance(transform.position, player.transform.position) > Atk3Range)) { Attack3(); }
-        if ((PhaseChange == true) && (AtkT <= 0f) && (Vector3.Distance(transform.position, player.transform.position) > Atk3Range)) { Attack3(); }
+        if ((PhaseChange == false) && (AtkT <= 0f) && (Vector3.Distance(transform.position, player.transform.position) > Atk3Range)) { Attack3(3f); }
+        if ((PhaseChange == true) && (AtkT <= 0f) && (Vector3.Distance(transform.position, player.transform.position) > Atk3Range)) { Attack3(2.5f); }
 
         if ((PhaseChange == false) && (AtkT <= 0f) && (Vector3.Distance(transform.position, player.transform.position) < Atk1Range)) { Attack2(Random.Range(1, 4)); }
         if ((PhaseChange == true) && (AtkT <= 0f) && (Vector3.Distance(transform.position, player.transform.position) < Atk1Range)) { Attack2(Random.Range(1, 4)); }
 
-        if ((PhaseChange == false) && (AtkT <= 0f) && (Vector3.Distance(transform.position, player.transform.position) > Atk1Range + 2 && (Vector3.Distance(transform.position, player.transform.position) < Atk2Range + 2))) { Attack1(3f); }
-        if ((PhaseChange == true) && (AtkT <= 0f) && (Vector3.Distance(transform.position, player.transform.position) > Atk1Range + 2 && (Vector3.Distance(transform.position, player.transform.position) < Atk2Range + 2))) { Attack1(2.5f); }
+        if ((PhaseChange == false) && (AtkT <= 0f) && (Vector3.Distance(transform.position, player.transform.position) > Atk1Range + 2 && (Vector3.Distance(transform.position, player.transform.position) < Atk2Range + 2))) { Attack1(2f); }
+        if ((PhaseChange == true) && (AtkT <= 0f) && (Vector3.Distance(transform.position, player.transform.position) > Atk1Range + 2 && (Vector3.Distance(transform.position, player.transform.position) < Atk2Range + 2))) { Attack1(1.25f); }
 
     }
 
@@ -72,22 +77,22 @@ public class SewerBossCombat : MonoBehaviour
         if (PhaseChange == false)
         {
             if (num == 1) { AtkT = 1.5f; anim.SetTrigger("Attack2"); navMeshAgent.speed = 1f; navMeshAgent.angularSpeed = 300f; }
-            if (num == 2) { AtkT = 2f; anim.SetTrigger("Attack5"); navMeshAgent.speed = 1f; navMeshAgent.angularSpeed = 200f; }
-            if (num == 3) { AtkT = 2f; anim.SetTrigger("Attack6"); navMeshAgent.speed = 1f; navMeshAgent.angularSpeed = 200f; }
+            if (num == 2) { AtkT = 1.25f; anim.SetTrigger("Attack5"); navMeshAgent.speed = 1f; navMeshAgent.angularSpeed = 200f; }
+            if (num == 3) { AtkT = 1f; anim.SetTrigger("Attack6"); navMeshAgent.speed = 1f; navMeshAgent.angularSpeed = 200f; }
         }
         else
         {
-            if (num == 1) { AtkT = 1.5f; anim.SetTrigger("Attack2"); navMeshAgent.speed = 1f; navMeshAgent.angularSpeed = 300f; }
-            if (num == 2) { AtkT = 1.5f; anim.SetTrigger("Attack5"); navMeshAgent.speed = 1f; navMeshAgent.angularSpeed = 300f; }
-            if (num == 3) { AtkT = 1.5f; anim.SetTrigger("Attack6"); navMeshAgent.speed = 1f; navMeshAgent.angularSpeed = 300f; }
+            if (num == 1) { AtkT = 1f; anim.SetTrigger("Attack2"); navMeshAgent.speed = 1f; navMeshAgent.angularSpeed = 500f; }
+            if (num == 2) { AtkT = 1f; anim.SetTrigger("Attack5"); navMeshAgent.speed = 1f; navMeshAgent.angularSpeed = 500f; }
+            if (num == 3) { AtkT = 1.25f; anim.SetTrigger("Attack6"); navMeshAgent.speed = 1f; navMeshAgent.angularSpeed = 500f; }
         }
         Debug.Log(num);
         attack3 = false;
     }
-    public void Attack3()
+    public void Attack3(float time)
     {
         attack3 = true;
-        AtkT = 3f;
+        AtkT = time;
         Atk3WaitT = 0f;
         navMeshAgent.speed = 0f; navMeshAgent.angularSpeed = 0f;
         anim.SetTrigger("Attack3");
